588eb2cafe7a600c2f5db52195b452cfa126260e
새ë¡황태웅 <>
2023-10-12 17:18:50 +0900

    엔티티, 엔티티Repository, 트랜잭션 엔티티 가공 10_12 day05

---
+++ src/main/java/example/day05/TodoEntity.java
@@ -0,0 +1,31 @@
+package example.day05;
+
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // JPA(ORM매핑) MYSQL 테이블과 매핑
+@Builder
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoEntity {
+
+    @Id // pk로 선정할 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    private int tno;
+    private String tcontent;
+    private boolean tstate;
+}
+
+/*
+    create table todo(
+        tno int auto_increment primary key ,
+        tcontent varchar(100) ,
+        tstate boolean
+    );
+*/
--- src/main/resources/static/todo.js
+++ src/main/resources/static/todo.js
@@ -1,30 +1,99 @@
 console.log('todo.js open');
 
-// 1.post ajax
-.ajax({
-    url:'/todo',
-    type:'post',
-    data:{},
-    success: r=>{console.log(r)},
+// 1. POST AJAX
+function Post1(){
+    let tcontent = document.querySelector('.tcontent').value;
+
+    $.ajax({
+        url: '/todo',
+        type: 'post',
+        data: { tno: 0, tcontent: tcontent, tstate: true },
+        success: r => {
+            if( r ){ alert('등록 성공'); }
+        },
+        error: e => { }
     })
-// 2.get ajax
-   .ajax({
-    url:'/todo',
-    type:'get',
-    data:{},
-    success:  r=>{console.log(r)} ,
+    tcontent = ''; // 등록 성공했으면 변수 초기화
+    get1(); // 새로고침
+} // f end
+
+get1(); // 웹 켜질때 한번 실행
+// 2. GET AJAX
+function get1(){
+    $.ajax({
+        url: '/todo',
+        type: 'get',
+        data: {},
+        success: r => { console.log(r);
+
+            let todo_bottom = document.querySelector('.todo_bottom'); // js 출력구역
+            let html = ``;
+
+            r.forEach( s =>{
+
+                if( s.tstate ){
+                     html += `
+                            <div class="todo">
+                                <div class="tcontent"> ${s.tcontent} </div>
+                                <div class="ectbtns">
+                                    <button onclick="put1( ${s.tno}, '${s.tcontent}', '${s.tstate}' )" type="button"> 상태변경 </button>
+                                    <button onclick="delete1( ${s.tno} )" type="button"> 제거하기 </button>
+                                </div>
+                            </div>`
+                        }
+                        else {
+                                html += `
+                                    <div class="todo successTodo">
+                                        <div class="tcontent"> ${s.tcontent} </div>
+                                        <div class="ectbtns">
+                                            <button onclick="put1( ${s.tno}, '${s.tcontent}', '${s.tstate}' )" type="button"> 상태변경 </button>
+                                            <button onclick="delete1( ${s.tno} )" type="button"> 제거하기 </button>
+                                        </div>
+                                    </div>`
+                       }
+            })
+             todo_bottom.innerHTML = html;
+        },
+        error: e => { }
     })
-// 3.put ajax
-.ajax({
-    url:'/todo',
-    type:'put',
-    data:{},
-    success:  r=>{console.log(r)},
-    })
-// 4.delete ajax
-.ajax({
-    url:'/todo',
-    type:'delete',
-    data:{},
-    success: r=>{console.log(r)} ,
-    })
+} // f end
+
+// 2. post
+
+
+// 3. put ajax
+function put1( tno, tcontent, tstate ){
+
+        $.ajax({
+            url: '/todo',
+            type: 'put',
+            data: { tno: tno, tcontent: tcontent, tstate: tstate },
+            success: r => {
+                if( r ){ console.log(r)
+                    alert('수정 성공');
+                }
+            },
+            error: e => { }
+        })
+    get1(); // 새로고침
+}
+
+// 4. delete ajax
+function delete1( tno ){
+
+        $.ajax({
+            url: '/todo',
+            type: 'delete',
+            data: { tno: tno, },
+            success: r => {
+
+                if( r ){
+                    console.log(r);
+                    alert('삭제 성공')
+                }
+            },
+            error: e => { }
+        })
+
+    get1(); // 새로고침
+}
